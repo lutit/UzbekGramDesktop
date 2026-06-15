0650fb55dc71bea0d6983edb7e24905ea221243b
Parsast <parsastdev@duck.com>
2026-06-15 17:17:17 +0330

    fix data_peer.cpp

--- Telegram/SourceFiles/data/data_peer.cpp
+++ Telegram/SourceFiles/data/data_peer.cpp
@@ -1301,9 +1301,9 @@ ChannelData *PeerData::broadcastMonoforum() const {
 }
 
 const QString &PeerData::topBarNameText() const {
-	if (AyuSettings::getInstance().haramMode) {
-		return AllahName();
-	}
+	// if (AyuSettings::getInstance().haramMode) {
+	// 	return AllahName();
+	// }
 	if (const auto to = migrateTo()) {
 		return to->topBarNameText();
 	} else if (const auto user = asUser()) {
@@ -1318,10 +1318,10 @@ int PeerData::nameVersion() const {
 	return _nameVersion;
 }
 
-// const QString &PeerData::name() const {
-// 	if (AyuSettings::getInstance().haramMode) {
-// 		return AllahName();
-// 	}
+const QString &PeerData::name() const {
+	// if (AyuSettings::getInstance().haramMode) {
+	// 	return AllahName();
+	// }
 	if (const auto to = migrateTo()) {
 		return to->name();
 	} else if (const auto broadcast = monoforumBroadcast()) {
@@ -1330,10 +1330,10 @@ int PeerData::nameVersion() const {
 	return _name;
 }
 
-// const QString &PeerData::shortName() const {
-// 	if (AyuSettings::getInstance().haramMode) {
-// 		return AllahName();
-// 	}
+const QString &PeerData::shortName() const {
+	// if (AyuSettings::getInstance().haramMode) {
+	// 	return AllahName();
+	// }
 	if (const auto user = asUser()) {
 		return user->firstName.isEmpty() ? user->lastName : user->firstName;
 	} else if (const auto to = migrateTo()) {
